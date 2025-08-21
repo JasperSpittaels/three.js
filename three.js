5e5ddf3f17da09bfcb7c1e7f8a2a22b3d06d2279
JasperSpittaels <168077782+JasperSpittaels@users.noreply.github.com>
2025-08-21 16:37:31 +0200

    Update three.js

--- three.js
+++ three.js
@@ -1,3 +1,3 @@
-import 'https://raw.githubusercontent.com/JasperSpittaels/three.js/refs/heads/main/libs/three.core.js';
-import 'https://raw.githubusercontent.com/JasperSpittaels/three.js/refs/heads/main/libs/three.module.js';
-import 'https://raw.githubusercontent.com/JasperSpittaels/three.js/refs/heads/main/libs/three.webgpu.js';
+import 'https://raw.githubusercontent.com/JasperSpittaels/three.js/main/libs/three.core.js';
+import 'https://raw.githubusercontent.com/JasperSpittaels/three.js/main/libs/three.module.js';
+import 'https://raw.githubusercontent.com/JasperSpittaels/three.js/main/libs/three.webgpu.js';
